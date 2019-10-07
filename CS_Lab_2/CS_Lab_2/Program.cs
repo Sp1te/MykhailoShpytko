2024446aa19022776aa3dd3e3b9ec125a58b28ea
Mykhailo Shpytko <mykhailoshpytko@gmail.com>
2019-10-07 11:24:57 +0300

    final

---
+++ CS_Lab_2/CS_Lab_2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+            Console.WriteLine("Type first string");
+            str1 = Console.ReadLine();
+           
+
+            while (true)
+            {
+                Console.WriteLine("Type second string");
+                str2 = Console.ReadLine();
+                if (str1.Length < str2.Length)
+                {
+                    Console.WriteLine("Second string must be equal or lesser than first string ");
+                }
+                else { break; }
+            }
+        
+            Console.WriteLine("str1 has content str2: " + ContentStr1InStr2(str1, str2));
+            Console.WriteLine(ChangeCharacter(str1, str2));
+            Console.WriteLine("str2 in str1 is contending: " + CoutOccurence(str1, str2));
+           
+
+
+            Console.ReadKey();
+        }
+
+
+        static bool ContentStr1InStr2(string str1, string str2)
+        {
+            int count = 0;
+
+            for(int i = 0; i < str1.Length; i++)
+            {
+                for(int j = 0; i < str2.Length; i++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        count++;
+
+                    } 
+                }
+            }
+            
+            return count == str2.Length;
+        }
+
+        static string ChangeCharacter(string str1, string str2)
+        {
+            StringBuilder strbuilder = new StringBuilder(str1);
+
+            for(int i = 0; i < strbuilder.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+
+                    if (strbuilder[i] == str2[j])
+                    {
+                        strbuilder[j] = '#';
+                    }
+
+                }
+            }
+            return Convert.ToString(strbuilder);
+        }
+
+        static int CoutOccurence(string str1, string str2)
+        {
+            
+            int count = (str1.Length - str2.Replace(str2, "").Length) / str2.Length;
+            
+            return count;
+        }
+
+    }
+}
---
+++ CS_Lab_3/CS_Lab_3/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int final = 100;
+            int size = 5;
+            Print_arr(Get_even_arr(size, start, final), size);
+            //Console.WriteLine(Search_max_value(Get_arr_rand(size), size));
+            //Console.WriteLine(Search_min_value(Get_arr_rand(size), size));
+
+            Console.ReadKey();
+        }
+
+
+        static int[,] Get_arr_rand(int size)
+        {
+            int[,] arr = new int[size, size];
+            Random rand = new Random();
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    if (i >= j)
+                    {
+
+                        arr[i, j] = rand.Next(1, 10);
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        static int[,] Get_even_arr(int size, int start, int final)
+        {
+            int[,] arr = new int[size, size];
+            Random rand = new Random();
+            int even = 0;
+            for(int i = 0; i < size; i++)
+            {
+            
+                for (int j = 0; j < size; j++)
+                {
+                    if ((rand.Next(start, final) % 2) == 0)
+                    {
+                        even = rand.Next(start, final);
+
+                        if (i >= j)
+                        {
+
+                            arr[i, j] = even;
+
+
+                        }
+                    }
+                }
+            }
+            return arr;
+
+        }
+
+        static int[,] Get_odd_arr(int size, int start, int final)
+        {
+            int[,] arr = new int[size, size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if ((rand.Next(start, final) % 2) == 1)
+                    {
+                        arr[i, j] = rand.Next(start, final);
+                    }
+                }
+            }
+            return arr;
+
+        }
+
+
+        static char[,] Get_arr_symbol(int size)
+        {
+            char[,] arr = new char[size, size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    if(i >= j)
+                    {
+                        arr[i, j] = (char)rand.Next(0x0020, 0x0070);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static char[,] Get__arr_eng(int size)
+        {
+            char[,] arr = new char[size, size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i >= j)
+                    {
+                        arr[i, j] = (char)rand.Next(0x0071, 0x007A);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static int Search_max_value(int[,] arr, int size)
+        {
+            int max = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    if(max < arr[i, j])
+                    {
+                        max = arr[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        static int Search_min_value(int[,] arr, int size)
+        {
+            int min = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (min > arr[i, j])
+                    {
+                        min = arr[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        static void Print_arr(int[,] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void Print_arr(char[,] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+
+   
+
+}
